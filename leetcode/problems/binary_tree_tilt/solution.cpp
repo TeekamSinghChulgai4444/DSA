e463a9024d9cd044cf8d27ae52f3abc7c4465b46
workteekamsingh4444-byte <work.teekam.singh.4444@gmail.com>
2025-09-04 13:29:23 +0000

    Sync LeetCode submission - Binary Tree Tilt (cpp)

---
+++ leetcode/problems/binary_tree_tilt/solution.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+  
+    int helper(TreeNode* root,int& sum){
+        if(root== NULL){
+            return 0;
+        }
+        int left=helper(root->left,sum);
+        int right=helper(root->right,sum);
+        sum+=abs(left-right);
+        return left+right+root->val;
+    }
+
+    int findTilt(TreeNode* root) {
+        
+        int sum=0;
+        helper(root,sum);
+        return sum;
+    }
+};
