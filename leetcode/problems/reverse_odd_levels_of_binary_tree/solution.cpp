304a00575c4085a15e93bb4a81e90e913d470807
workteekamsingh4444-byte <work.teekam.singh.4444@gmail.com>
2025-10-05 11:47:37 +0000

    Sync LeetCode submission - Reverse Odd Levels of Binary Tree (cpp)

---
+++ leetcode/problems/reverse_odd_levels_of_binary_tree/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void dfs(TreeNode* left, TreeNode* right, int level) {
+        if (!left || !right) return;
+
+        if (level % 2 == 1) {
+            swap(left->val, right->val);
+        }
+
+        dfs(left->left, right->right, level + 1);
+        dfs(left->right, right->left, level + 1);
+    }
+
+    TreeNode* reverseOddLevels(TreeNode* root) {
+        if (!root) return nullptr;
+        dfs(root->left, root->right, 1);
+        return root;
+    }
+};
