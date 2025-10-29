fcc6414867108522f82581d8d713afd702791474
workteekamsingh4444-byte <work.teekam.singh.4444@gmail.com>
2025-10-29 11:34:05 +0000

    Sync LeetCode submission - Sort an Array (cpp)

--- leetcode/problems/sort_an_array/solution.cpp
+++ leetcode/problems/sort_an_array/solution.cpp
@@ -1,43 +1,47 @@
 class Solution {
 public:
-    void merge(vector<int>& nums, int ft, int mid, int end) {
-        vector<int> temp;
-        int i = ft;
-        int j = mid + 1;
 
-        while (i <= mid && j <= end) {
-            if (nums[i] <= nums[j]) {
-                temp.push_back(nums[i]);
-                i++;
-            } else {
-                temp.push_back(nums[j]);
+    void merge(vector<int>&nums,int st,int mid,int  end){
+        vector<int>ans;
+        int i=st;
+        int j=mid+1;
+
+        while(i<=mid && j<=end){
+            if(nums[i]>nums[j]){
+                ans.push_back(nums[j]);
                 j++;
+            }else{
+                ans.push_back(nums[i++]);
             }
         }
 
-        while (i <= mid) {
-            temp.push_back(nums[i++]);
-        }
-        while (j <= end) {
-            temp.push_back(nums[j++]);
+        while(i<=mid){
+            ans.push_back(nums[i++]);
         }
 
-        for (int k = 0; k < temp.size(); k++) {
-            nums[ft + k] = temp[k];
+        while(j<=end){
+            ans.push_back(nums[j++]);
+        }
+
+        for(int k=0;k<ans.size();k++){
+            nums[k+st]=ans[k];
         }
     }
+    void helper(vector<int>&nums,int st,int end){
 
-    void mergeSort(vector<int>& nums, int ft, int end) {
-        if (ft < end) {
-            int mid = ft + (end - ft) / 2;
-            mergeSort(nums, ft, mid);
-            mergeSort(nums, mid + 1, end);
-            merge(nums, ft, mid, end);
+        if(st<end){
+        int mid = st + (end - st) / 2;
+        helper(nums,st,mid);
+        helper(nums,mid+1,end);
+        merge(nums,st,mid,end);
         }
-    }
 
+    }
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size() - 1);
-        return nums;
+            int st=0;
+            int end=nums.size()-1;
+            helper(nums,st,end);
+            return nums;
+        
     }
 };
