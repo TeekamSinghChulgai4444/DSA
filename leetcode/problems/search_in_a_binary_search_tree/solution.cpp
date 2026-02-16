ccd4d8f1e42fcfe68a39c8273fc52fbbc5b59470
workteekamsingh4444-byte <work.teekam.singh.4444@gmail.com>
2026-02-16 12:40:24 +0000

    Sync LeetCode submission - Search in a Binary Search Tree (cpp)

---
+++ leetcode/problems/search_in_a_binary_search_tree/solution.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int d) {
+
+        if(root==NULL){
+            return NULL;
+        }
+        if(root->val == d){
+            return root;
+        }
+        if(root->val > d ){
+            return searchBST(root->left,d);
+        }
+        else if( root->val < d){
+            return searchBST(root->right,d);
+        }
+        return NULL;
+        
+    }
+};
