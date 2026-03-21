68f0bd75348dc158971ef540ebb777537c3d0489
workteekamsingh4444-byte <work.teekam.singh.4444@gmail.com>
2026-03-21 11:37:35 +0000

    Sync LeetCode submission - Longest Palindromic Substring (java)

---
+++ leetcode/problems/longest_palindromic_substring/solution.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String longestPalindrome(String s) {
+
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        int l = 0, m = 0;
+
+        for (int k = 0; k < n; k++) {
+            int i = 0;
+            int j = k;
+
+            while (j < n) {
+
+                if (i == j) {
+                    dp[i][j] = 1;
+                } else if (j == i + 1) {
+                    if (s.charAt(i) == s.charAt(j)) {
+                        dp[i][j] = 1;
+                        l = i;
+                        m = j;
+
+                    }
+                } else {
+                    if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1] == 1) {
+                        dp[i][j] = 1;
+                        l = i;
+                        m = j;
+                    }
+                }
+
+                i++;
+                j++;
+            }
+        }
+
+        return s.substring(l, m + 1);
+    }
+
+}
