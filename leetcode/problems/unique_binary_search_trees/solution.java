8a74f97ad422f9c9b1e3d1b7c66d9882dac7a1dc
workteekamsingh4444-byte <work.teekam.singh.4444@gmail.com>
2026-03-22 02:21:25 +0000

    Sync LeetCode submission - Unique Binary Search Trees (java)

---
+++ leetcode/problems/unique_binary_search_trees/solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int numTrees(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = 0; 
+
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+
+        return dp[n];
+    }
+}
