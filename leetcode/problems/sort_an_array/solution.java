55608ad2f5ea267df72199c19c2e082d9c84b3a8
workteekamsingh4444-byte <work.teekam.singh.4444@gmail.com>
2026-04-16 02:20:26 +0000

    Sync LeetCode submission - Sort an Array (java)

---
+++ leetcode/problems/sort_an_array/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();  
+
+        for (int i : nums) {
+            pq.offer(i);
+        }
+
+        for (int i = 0; i < nums.length; i++) { 
+            nums[i] = pq.poll();                 
+        }
+
+        return nums;
+    }
+}
