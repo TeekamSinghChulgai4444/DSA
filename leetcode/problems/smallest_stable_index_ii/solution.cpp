d3650281a4572f33991ea372f5394c66421bec92
workteekamsingh4444-byte <work.teekam.singh.4444@gmail.com>
2026-04-19 03:06:12 +0000

    Sync LeetCode submission - Smallest Stable Index II (cpp)

---
+++ leetcode/problems/smallest_stable_index_ii/solution.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+
+        int n=nums.size();
+
+        vector<int>left(n);
+        vector<int>right(n);
+
+        int maxx=INT_MIN;
+
+        for(int i=0;i<n;i++){
+            maxx=max(maxx, nums[i]);
+            left[i]=maxx;
+        }
+
+        int minn=INT_MAX;
+
+        for(int i=n-1;i>=0;i--){
+            minn=min(minn,nums[i]);
+            right[i]=minn;
+        }
+
+        int check=INT_MAX;
+        int idx=-1;
+
+        for(int i=0;i<n;i++){
+            int ans=left[i]-right[i];
+
+            if(ans<=k){
+               return i;
+            }
+        }
+
+       
+        return -1;
+    }
+};
